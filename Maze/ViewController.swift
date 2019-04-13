6efed8baaebd25c71550ea44fb647c5111359249
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-13 20:46:42 +0900

    マップを作った

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     //画面サイズの取得
     let screenSize = UIScreen.main.bounds.size
     
+    //迷路のマップを表してる
     let maze = [
         [1, 0, 0, 0, 1, 0],
         [1, 0, 1, 0, 1, 0],
@@ -26,10 +27,57 @@ class ViewController: UIViewController {
         [0, 0, 1, 1, 1, 2],
     ]
     
+    var startView: UIView!
+    var goalView: UIView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //マップ1マスの幅，高さ
+        let cellWidth = screenSize.width / CGFloat(maze[0].count)
+        let cellHeight = screenSize.height / CGFloat(maze.count)
+        
+        //マスの左上と中心の座標の差
+        let cellOffsetX = screenSize.width / CGFloat(maze[0].count * 2)
+        let cellOffsetY = screenSize.height / CGFloat(maze.count * 2)
+        
+        //壁，スタート地点，ゴール地点を設置
+        for y in 0 ..< maze.count {
+            for x in 0 ..< maze[y].count {
+                switch maze[y][x]{
+                case 1:
+                    let wallView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    wallView.backgroundColor = UIColor.black
+                    view.addSubview(wallView)
+                case 2:
+                    startView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    startView.backgroundColor = UIColor.green
+                    view.addSubview(startView)
+                case 3:
+                    goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOffsetY)
+                    goalView.backgroundColor = UIColor.red
+                    view.addSubview(goalView)
+                default:
+                    break
+                }
+            }
+        }
+        
     }
+    
+    //迷路の１マスとなるUIViewを作るメソッド
+    func createView(x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat, offsetY: CGFloat) -> UIView {
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        let view = UIView(frame: rect)
+        
+        let center = CGPoint(x: offsetX + width * CGFloat(x), y: offsetY + height * CGFloat(y))
+        
+        view.center = center
+        
+        return view
+    }
+
 
 
 }
